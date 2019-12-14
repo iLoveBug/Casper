7f0cce5f4774387204b1cd2ac7039c3c0c8c5d9c
iLoveBug <fan.zhang@me.com>
2019-12-14 21:19:50 +0800

    增加为基于vue的卡片自动创建Vue实例（需定制vue-card.js文件） 引入最新的locations-list相关库文件（min.js + css） 删除以前的locations-view.js文件 增加编译后的结果 解决eslint提示Vue变量未定义的问题 主题模版默认加载vue.js和location-list.css文件（js文件已合并入casper.js文件中）

---
+++ assets/js/vue-card.js
@@ -0,0 +1,19 @@
+/* eslint-env browser */
+
+/**
+ * vue-based card support
+ * Used on any individual post/page
+ *
+ * Detects when a vue-based card has been used and applies vue to make sure it works.
+ */
+
+(function (window, document) {
+    var applyVueForCards = function applyVueForCards() {
+        var cards = document.querySelectorAll('.kg-locations-container');
+        cards.forEach(function (card) {
+            new Vue({el: card});
+        });
+    };
+
+    document.addEventListener('DOMContentLoaded', applyVueForCards);
+})(window, document);
